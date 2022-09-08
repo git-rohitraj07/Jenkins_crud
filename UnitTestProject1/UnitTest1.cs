2bebe38dc1165fdfaa00b9aff5fb2b0bb472fb33
Rohit Gangwal <rohitgangwal0@gmail.com>
2022-09-08 11:21:55 +0530

    test

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Web.Mvc;
+using WebApplication7.Controllers;
+using WebApplication7.Models;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Index()
+        {
+            EmployeeController controller = new EmployeeController();
+
+            ViewResult result = controller.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Add()
+        {
+            EmployeeController controller = new EmployeeController();
+            ViewResult result = controller.Add(0) as ViewResult;
+            Assert.IsNotNull(result);
+        }
+    }
+}
